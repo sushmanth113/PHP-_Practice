5352eeac094c43b6f71569c78a6e66ecd7165441
Sushmanth Karnam <sushmanth.karnam@aspiresys.com>
2021-09-16 21:03:39 +0530

    Php pratices

---
+++ precedence operators.php
@@ -0,0 +1,6 @@
+<?php
+$x = 4;
+echo "x minus one equals " . $x-1 . ", or so I hope\n";
+echo (("x minus one equals " . $x) - 1) . ", or so I hope\n";
+echo "x minus one equals " . ($x-1) . ", or so I hope\n";
+?>
